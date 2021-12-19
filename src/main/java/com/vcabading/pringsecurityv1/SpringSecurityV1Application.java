a09f8d9a8a0101122e81f48fba893a71485a9bf6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-18 22:44:43 -0800

    initial commit

---
+++ src/main/java/com/vcabading/pringsecurityv1/SpringSecurityV1Application.java
@@ -0,0 +1,13 @@
+package com.vcabading.pringsecurityv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcabading/pringsecurityv1/SpringSecurityV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.pringsecurityv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
